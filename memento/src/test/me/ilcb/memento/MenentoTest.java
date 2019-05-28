281007329f827b9654395a4d06329ba81ca38cf1
ilcbba <ilcbba@126.com>
2019-05-28 20:57:01 +0800

    备忘录模式

--- memento/src/test/me/ilcb/memento/MenentoTest.java
+++ memento/src/test/me/ilcb/memento/MenentoTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class MenentoTestTest {
+public class MenentoTest {
     @Test
     public void test() {
         Originator originator = new Originator();
